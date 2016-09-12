597b946aedf3b6bfd1cc1dc97a0e1120f74d461a
redvox27 <v.t.luder@st.hanze.nl>
2016-09-12 16:25:57 +0200

    opgave 1 toegevoegd

---
+++ opgave1.py
@@ -0,0 +1,28 @@
+#opdracht A
+lijstA = [2,3,4];
+
+#opdracht B
+lijstB = ["red", "green", "blue"];
+
+#opdracht C
+lijstC = [3,4,5];
+lengLijstC = len(lijstC);
+
+#opdracht D
+lijstD = ['a', 'b', 'c', 'd'];
+
+#OUTPUT VAN DE OPDRACHTEN
+
+#opdracht A
+print(lijstA);
+
+#opdracht B
+print(lijstB);
+
+
+#opdracht C
+for i in range(0,lengLijstC) :
+    print(lijstC[i]);
+
+#opdracht D
+print(lijstD);
